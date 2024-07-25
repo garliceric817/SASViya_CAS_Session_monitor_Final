bad9a520c62997ee5b3c46d9b080f0f41209680c
lichku <garliceric817@gmail.com>
2024-07-25 14:47:34 +0800

    2nd commit

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import requests
-from function import get_bearer_token
+from function import get_bearer_token, get_cas_info
 
-BT = get_bearer_token("tksviya.unx.sas.com", "Jask", "demopw")
+endpoint = "tksviya.unx.sas.com"
+cas_endpoint = "tksviya.unx.sas.com"
+
+# Get Bearer Token
+BT = get_bearer_token(endpoint, "Jask", "demopw")
+# Define CAS API URL
+cas_path = "cas/nodes"
+cas_port = 47384
+
+# Get every CAS nodes' status
+cas_node_status = get_cas_info(cas_endpoint, cas_port, BT)
+
 
 
 
