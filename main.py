0936fdbae9f2cba48d0632bb8e027bb49f8385db
lichku <garliceric817@gmail.com>
2024-07-29 21:58:40 +0800

    3rd commit

--- main.py
+++ main.py
@@ -1,17 +1,84 @@
 import requests
-from function import get_bearer_token, get_cas_info
+from viya_function import *
+from prometheus_client import Gauge, Counter, start_http_server
+import schedule
+import time
 
 endpoint = "tksviya.unx.sas.com"
 cas_endpoint = "tksviya.unx.sas.com"
 
-# Get Bearer Token
+# Viya - Get Bearer Token
 BT = get_bearer_token(endpoint, "Jask", "demopw")
 # Define CAS API URL
-cas_path = "cas/nodes"
-cas_port = 47384
+# cas_path = "cas/nodes"
+cas_port = 47384 #47839
 
-# Get every CAS nodes' status
-cas_node_status = get_cas_info(cas_endpoint, cas_port, BT)
+# define prmetheus metrics
+cas_node_status_gauge = Gauge('cas_server_health', 'Indicates server health (1 for healthy, 0 for unhealthy)', ['cas_node'])
+session_cpu_usage_gauge = Gauge('cas_session_cpu', 'CPU usage of sessions', ['session_name', 'node_type'])
+session_mem_usage_gauge = Gauge('mem_session_cpu', 'MEM usage of sessions', ['session_name', 'node_type'])
+
+
+def fetch_and_update_prometrics():
+    cas_node_status = get_cas_info(cas_endpoint, cas_port, BT)
+    print(cas_node_status)
+    for server in cas_node_status:
+        print(cas_node_status[server])
+        cas_node_status_gauge.labels(cas_node=server).set(1 if cas_node_status[server] is True else 0)
+    return
+
+
+# get CPU & Mem usage of each session.
+session_list = get_session(cas_endpoint, cas_port, BT)
+print(session_list)
+# for loop for each Session
+for session_name in session_list:
+    session_on_node = get_session_node(cas_endpoint, cas_port, BT, session_list[session_name])
+
+    controller_cpu_sum = 0
+    bk_controller_cpu_sum = 0
+    worker_cpu_sum = 0
+    controller_mem_sum = 0
+    bk_controller_mem_sum = 0
+    worker_mem_sum = 0
+
+    # for loop for each PID on a node
+    for node in session_on_node:
+        cpu_mem_list = get_grid_info(cas_endpoint, cas_port, BT, node, session_on_node[node])
+        if "controller" in node:
+            controller_cpu_sum += cpu_mem_list[0]
+            controller_mem_sum += cpu_mem_list[1]
+        elif "backup" in node:
+            bk_controller_cpu_sum += cpu_mem_list[0]
+            bk_controller_mem_sum += cpu_mem_list[1]
+        elif "worker" in node:
+            worker_cpu_sum += cpu_mem_list[0]
+            worker_mem_sum += cpu_mem_list[1]
+    # Have gotten the total CPU, Mem usage of a Session.Then update the Prometheus Metrics
+    session_cpu_usage_gauge.labels(session_name=session_name, node_type="controller").set(controller_cpu_sum)
+    session_cpu_usage_gauge.labels(session_name=session_name, node_type="backup-controller").set(bk_controller_cpu_sum)
+    session_cpu_usage_gauge.labels(session_name=session_name, node_type="worker").set(worker_cpu_sum)
+
+    session_mem_usage_gauge.labels(session_name=session_name, node_type="controller").set(controller_mem_sum)
+    session_mem_usage_gauge.labels(session_name=session_name, node_type="backup-controller").set(bk_controller_mem_sum)
+    session_mem_usage_gauge.labels(session_name=session_name, node_type="worker").set(worker_mem_sum)
+
+
+# get_grid_info(cas_endpoint, cas_port, BT, "worker-0.sas-cas-server-default.viya4deploy.svc.cluster.local", 244571)
+
+
+# schedule.every(30).seconds.do(fetch_and_update_prometrics)
+#
+# if __name__ == "__main__":
+#     # Start Prometheus HTTP server
+#     start_http_server(8000)  # Port where Prometheus will scrape metrics
+#
+#     # Schedule job
+#     while True:
+#         schedule.run_pending()
+#         time.sleep(1)
+
+
 
 
 
---
+++ viya_function.py
@@ -0,0 +1,109 @@
+import requests
+import json
+
+import http.client
+import ssl
+
+
+def get_bearer_token(sas_server, user, pw):
+    url = f"https://{sas_server}/SASLogon/oauth/token"
+    payload = f"grant_type=password&username={user} &password={pw}"
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic c2FzLmNsaTo='
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+    bearer_token = json.loads(response.text)['access_token']
+    return bearer_token
+
+
+def get_cas_info(cas_server, port, bt):
+    path = "cas/nodes"
+    url = f"https://{cas_server}:{port}/{path}"
+
+    headers = {
+        'Authorization': f"Bearer {bt}",
+        'Accept': "application/vnd.sas.collection+json"
+    }
+
+    # Send the GET request with SSL verification disabled
+    response = requests.get(url, headers=headers, verify=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        # uuids = [item['uuid'] for item in data if 'uuid' in item]
+        # print(uuids)
+        node_connect_status = {item['name']: item['connected'] for item in data}
+        print(node_connect_status)
+        # print(data)
+        return node_connect_status
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return
+
+
+def get_session(cas_server, port, bt):
+    path = "cas/sessions"
+    url = f"https://{cas_server}:{port}/{path}"
+
+    headers = {
+        'Authorization': f"Bearer {bt}",
+        'Accept': "application/vnd.sas.collection+json"
+    }
+
+    # Send the GET request with SSL verification disabled
+    response = requests.get(url, headers=headers, verify=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        # print(data)
+        session_list = {session_info["name"]: session_info["uuid"] for session_info in data}
+        return session_list
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return
+
+
+def get_session_node(cas_server, port, bt, uuid):
+    path = f"cas/sessions/{uuid}/nodes/metrics"
+    url = f"https://{cas_server}:{port}/{path}"
+
+    headers = {
+        'Authorization': f"Bearer {bt}",
+        'Accept': "application/vnd.sas.collection+json"
+    }
+
+    # Send the GET request with SSL verification disabled
+    response = requests.get(url, headers=headers, verify=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        pid_on_node = {chunk['name']: chunk['pid'] for chunk in data}
+
+        return pid_on_node
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return
+
+
+def get_grid_info(cas_server, port, bt, node_name, pid):
+    path = f"grid/{node_name}/processes/{pid}"
+    url = f"https://{cas_server}:{port}/{path}"
+
+    headers = {
+        'Authorization': f"Bearer {bt}",
+        'Accept': "application/vnd.sas.collection+json"
+    }
+
+    # Send the GET request with SSL verification disabled
+    response = requests.get(url, headers=headers, verify=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        cpu = data["cpu"]
+        mem = data["vmSize"]
+        return (cpu, mem)
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return
