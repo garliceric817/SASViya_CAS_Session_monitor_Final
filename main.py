6b4de437e9ea86f5d57870f52bb87d879a2d36db
lichku <garliceric817@gmail.com>
2024-08-16 18:26:23 +0800

    4rd commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from viya_function import *
-from prometheus_client import Gauge, Counter, start_http_server
+from prometheus_client import Gauge, Counter, start_http_server, CollectorRegistry
 import schedule
 import time
 
@@ -12,6 +12,8 @@ BT = get_bearer_token(endpoint, "Jask", "demopw")
 # Define CAS API URL
 # cas_path = "cas/nodes"
 cas_port = 47384 #47839
+
+
 
 # define prmetheus metrics
 cas_node_status_gauge = Gauge('cas_server_health', 'Indicates server health (1 for healthy, 0 for unhealthy)', ['cas_node'])
@@ -29,22 +31,22 @@ def fetch_and_update_prometrics():
 
 # get CPU & Mem usage of each session.
 def fetch_and_update_prometrics_cpu_mem():
-    try:
-        session_list = get_session(cas_endpoint, cas_port, BT)
-        print(session_list)
-        # for loop for each Session
-        for session_name in session_list:
-            session_on_node = get_session_node(cas_endpoint, cas_port, BT, session_list[session_name])
-            print(session_on_node)
+    session_list = get_session(cas_endpoint, cas_port, BT)
+    print(session_list)
+    # for loop for each Session
+    for session_name in session_list:
+        session_on_node = get_session_node(cas_endpoint, cas_port, BT, session_list[session_name])
+        print(session_on_node)
 
-            controller_cpu_sum = 0
-            bk_controller_cpu_sum = 0
-            worker_cpu_sum = 0
-            controller_mem_sum = 0
-            bk_controller_mem_sum = 0
-            worker_mem_sum = 0
+        controller_cpu_sum = 0
+        bk_controller_cpu_sum = 0
+        worker_cpu_sum = 0
+        controller_mem_sum = 0
+        bk_controller_mem_sum = 0
+        worker_mem_sum = 0
 
-            # for loop for each PID on a node
+        try:
+        # for loop for each PID on a node
             for node in session_on_node:
                 cpu_mem_list = get_grid_info(cas_endpoint, cas_port, BT, node, session_on_node[node])
                 # [0] = CPU, [1] = MEM
@@ -70,14 +72,29 @@ def fetch_and_update_prometrics_cpu_mem():
             # except requests.RequestException as e:
             #     print(f"Error fetching data from {session_name}: {e}")
             #     continue
-    except:
-        print("[Error] - Failed to Fetch")
+        except:     # Set all metrics value = 0 of disappeared session
+            print(f"Session - {session_name} is gone, set all metrics = 0")
+            node_type = ["controller", "backup-controller", "worker"]
+            for node in node_type:
+                session_cpu_usage_gauge.labels(session_name=session_name, node_type=node).set(0)
+                session_mem_usage_gauge.labels(session_name=session_name, node_type=node).set(0)
+
+    # Set all metrics value = 0 of disappeared session
+    session_list_final = get_session(cas_endpoint, cas_port, BT)
+    for session_name in session_list:
+        if session_name not in session_list_final:
+            node_type = ["controller", "backup-controller", "worker"]
+            for node in node_type:
+                session_cpu_usage_gauge.labels(session_name=session_name, node_type=node).set(0)
+                session_mem_usage_gauge.labels(session_name=session_name, node_type=node).set(0)
+
     return
 # get_grid_info(cas_endpoint, cas_port, BT, "worker-0.sas-cas-server-default.viya4deploy.svc.cluster.local", 244571)
 
 
-schedule.every(30).seconds.do(fetch_and_update_prometrics)
-schedule.every(30).seconds.do(fetch_and_update_prometrics_cpu_mem)
+
+#schedule.every(30).seconds.do(fetch_and_update_prometrics)
+#schedule.every(30).seconds.do(fetch_and_update_prometrics_cpu_mem)
 
 if __name__ == "__main__":
     # Start Prometheus HTTP server
@@ -85,6 +102,11 @@ if __name__ == "__main__":
 
     # Schedule job
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        fetch_and_update_prometrics()
+        fetch_and_update_prometrics_cpu_mem()
+        time.sleep(30)
+
+
+
+
 
